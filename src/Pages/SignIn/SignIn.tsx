008ee6648ddf5caf64b3b43f4bb12f1bba3d908f
Cherkasant <antnchrks@gmail.com>
2023-10-10 22:17:14 +0300

    added checkToken

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,6 +1,21 @@
 import Form from '../../Components/Form';
+import { TOKEN_KEY } from '../../Redux/utils/api';
+import { setLoggedIn } from '../../Redux/Reducers/authReducer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { PathNames } from '../Router/Router';
+import { useNavigate } from 'react-router';
 
 const SignIn = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem(TOKEN_KEY)) {
+      dispatch(setLoggedIn(true));
+      navigate(PathNames.Catalog);
+    }
+  }, []);
   return (
     <div>
       <Form />
